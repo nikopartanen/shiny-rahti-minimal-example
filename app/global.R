c52dfeaea2821b273311bdf1273d22d3367009f1
Niko Partanen <nikotapiopartanen@gmail.com>
2025-03-03 19:43:50 +0200

    Create global.R

---
+++ app/global.R
@@ -0,0 +1,10 @@
+# Load required packages
+library(shiny)
+
+# Add any other packages your app needs
+# library(ggplot2)
+# library(dplyr)
+# library(tidyr)
+
+# Define any global variables or functions
+appName <- "Minimal Shiny App"
